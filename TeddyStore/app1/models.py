410170c9a1d1afde794293f749820e6a660f833c
mich0rtega <156623518+mich0rtega@users.noreply.github.com>
2024-10-14 08:39:37 -0600

    BACK END| Modelo de tablas

---
+++ TeddyStore/app1/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+
+#Tabla usuarios
+class Usuarios(models.Model):
+    usuario = models.AutoField(primary_key=True)
+    password = models.CharField(max_length=8)
+    direccion = models.CharField(max_length=100)
+    telefono = models.IntegerField(max_length=10)
+
+    def __str__(self):
+        return self.usuario
+    
+#Tabla Productos
+class Productos(models.Model):
+    cod_producto = models.IntegerField(primary_key=True)
+    nombre = models.TextField(max_length=40)
+    descripcion = models.CharField(max_length=100)
+    precio = models.DecimalField(max_digits=10,decimal_places=2)
+    stock = models.CharField(max_length=100)
+    imagen = models.ImageField(upload_to='image/',)
+   
+
+    def __str__(self):
+        return self.nombre
+    
